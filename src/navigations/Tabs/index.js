99b50726e1fcfa396e6bf060027dde3ea4ce8288
Ismail Can Vardar <ismailcanvardar@gmail.com>
2022-02-14 02:41:02 +0300

    add home screen components

--- src/navigations/Tabs/index.js
+++ src/navigations/Tabs/index.js
@@ -5,6 +5,8 @@ import ScanScreen from "../../screens/Scan";
 import HistoryScreen from "../../screens/History";
 import SettingsScreen from "../../screens/Settings";
 
+import { Ionicons } from "@expo/vector-icons";
+
 import { THEME } from "../../constants";
 
 const Tab = createBottomTabNavigator();
@@ -13,7 +15,37 @@ const Tabs = () => {
   return (
     <NavigationContainer theme={THEME}>
       <Tab.Navigator
-        screenOptions={{ headerShown: false, tabBarShowLabel: false }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Scan") {
+              iconName = focused ? "scan" : "scan-outline";
+            } else if (route.name === "History") {
+              iconName = focused ? "time" : "time-outline";
+            } else if (route.name === "Settings") {
+              iconName = focused ? "settings" : "settings-outline";
+            }
+
+            // You can return any component that you like here!
+            return (
+              <Ionicons
+                name={iconName}
+                size={size}
+                color={THEME.colors.primary}
+              />
+            );
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            borderTopColor: THEME.colors.primary,
+          },
+        })}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Scan" component={ScanScreen} />
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,38 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  FlatList,
+  Image,
+  ScrollView
+} from "react-native";
+import React, { useEffect } from "react";
+import CustomText from "../../components/CustomText";
 
 import useStore from "../../store/useStore";
 import Container from "../../components/Container";
+import { useTheme } from "@react-navigation/native";
+import { SIZES } from "../../constants";
+
+const productsData = {
+  image: "QmZSTNGaecoEjogJD1zw6qnh2hZL26WvfjPHkYeeH87ZCE",
+  name: "Consensux",
+  type: "Watch",
+  specifications: ["Rose Gold", "Water proof to 30m"],
+  details:
+    "Manually wound mechanical movement. Caliber CHR 29‑535 PS Q. Split-Seconds ch",
+  info: {
+    amount: 10,
+    year: 1990,
+    country: "Italy",
+  },
+};
+
+const productsArr = Array(5).fill(productsData);
 
 const Home = () => {
+  const { colors } = useTheme();
   const removeWallet = useStore((store) => store.removeWallet);
   const removePassword = useStore((store) => store.removePassword);
   const checkWallet = useStore((store) => store.checkWallet);
@@ -15,13 +43,79 @@ const Home = () => {
     await checkWallet();
   };
 
+  const ProductCard = () => {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.windowWidth / 18,
+          height: SIZES.windowWidth / 0.75,
+          width: "100%",
+          backgroundColor: colors.backgroundSecondary,
+          borderRadius: 24,
+          justifyContent: "center",
+          paddingBottom: SIZES.windowWidth / 10
+        }}
+      >
+        <View
+          style={{
+            paddingHorizontal: SIZES.paddingHorizontal,
+            paddingBottom: SIZES.windowWidth / 12,
+          }}
+        >
+          <CustomText style={{ fontSize: SIZES.h2, color: colors.text }}>
+            Consensux
+          </CustomText>
+          <CustomText style={{ fontSize: SIZES.h5, color: colors.text }}>
+            Watch
+          </CustomText>
+        </View>
+        <Image
+          style={{ height: SIZES.windowWidth / 2, zIndex: 1 }}
+          resizeMode="contain"
+          source={{
+            uri: `https://ipfs.io/ipfs/QmZSTNGaecoEjogJD1zw6qnh2hZL26WvfjPHkYeeH87ZCE`,
+          }}
+        />
+        <View
+          style={{
+            alignItems: "center",
+            position: "absolute",
+            bottom: 0,
+            left: 12,
+          }}
+        >
+          <Image
+            resizeMode="stretch"
+            source={require("../../../assets/images/glitters.png")}
+          />
+        </View>
+      </View>
+    );
+  };
+
   return (
     <Container>
       <TouchableOpacity onPress={handleClick}>
         <Text>Remove Wallet</Text>
       </TouchableOpacity>
+      <View>
+        <CustomText
+          style={{ color: colors.text, fontSize: SIZES.h3 }}
+          fontWeight="bold"
+        >
+          My Wallet
+        </CustomText>
+      </View>
+      {/* Product Card */}
+      <ProductCard />
+      {/* Products List */}
+      <FlatList />
     </Container>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  headerContainer: {},
+});
